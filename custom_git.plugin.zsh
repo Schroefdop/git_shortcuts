6a2c920369d593ed69696ef43c186fe75904ccea
Schroefdop <woutervermeij@hotmail.com>
2021-03-03 17:11:10 +0100

    Update custom_git.plugin.zsh

--- custom_git.plugin.zsh
+++ custom_git.plugin.zsh
@@ -25,7 +25,7 @@ alias gda='git diff HEAD'
 alias gf='git fetch'
 alias glo='git log --oneline'
 alias glg='git log --graph --oneline --decorate --all'
-alias glp='git log --pretty=format:"%Cred%h%Creset|%Cgreen%ad%Creset|%Cgreen%cr%Creset|%C(bold blue)<%an>%Creset"\n"%C(yellow)%d%Creset %s" --abbrev-commit --date=short' # Print git log data on two line
+alias glp='git log --pretty=format:"%Cred%h%Creset|%Cgreen%ad%Creset|%Cgreen%cr%Creset|%C(bold blue)<%an>%Creset""%C(yellow)%d%Creset %s" --abbrev-commit --date=short' # Print git log data on two line
 alias gm='git merge --no-ff'
 alias gmd='git merge --no-ff develop'
 alias gma='git merge --abort'
@@ -56,7 +56,7 @@ glf() {
 # Git push local branch to equally named origin branch
 gpb() {
   currentBranch=$(git branch | grep \* | cut -d ' ' -f2)
-  git push origin $currentBranch
+  git push -u origin $currentBranch
 }
 
 # Clear all stashes
@@ -76,15 +76,28 @@ gstc() {
   esac
 }
 
-# Clean the remote branches
-gbrp() {
-  echo "Branches before clean: "
-  gbr
-  echo "Cleaning remote..."
+# Clean remote/local branches
+grp() {
   git remote update origin --prune
-  echo "Done 👍"
-  echo "Branches after clean: "
-  gbr
+
+  NUMBER_OF_BRANCHES_REMOVED=$(git branch -vv | grep -c ': gone]')
+
+  if [[ $NUMBER_OF_BRANCHES_REMOVED > 0 ]]; then
+
+    printf "${RED}Prune local branches?${NOCOLOR} [y/n] "
+
+    read confirm
+    case "$confirm" in
+    [yY])
+      git branch -vv | grep ': gone]' | grep -v '\*' | awk '{ print $1; }' | xargs git branch -D
+      ;;
+    *)
+      echo "¯\_(ツ)_/¯"
+      ;;
+    esac
+  else
+    echo "${RED}No local branches to prune${NOCOLOR}"
+  fi
 }
 
 # Reset current branch to equal named origin branch
@@ -100,89 +113,4 @@ gbreset() {
     echo "¯\_(ツ)_/¯"
     ;;
   esac
-}
-
-gco() {
-  _listBranches "checkout"
-}
-
-gbd() {
-  _listBranches "deletion"
-}
-
-gcor() {
-  _listBranches "remote checkout"
-}
-
-_listBranches() {
-  branchesFile=$TMPDIR'branches'
-  trap "{ rm -f $branchesFile; }" EXIT
-
-  case $1 in
-  "remote checkout") git branch -r >$branchesFile ;; # Get remote branches
-  *) git branch >$branchesFile ;;                    # Get local branches
-  esac
-
-  n=1
-  while read line; do
-    if [[ $line == *"*"* ]]; then
-      currentBranch=$(git branch | grep \* | cut -d ' ' -f2)
-      echo "Current branch:\n${GREEN}$currentBranch${NOCOLOR}"
-      # Remove the current branch from the file, we cannot checkout current branch
-      sed -i "" $n\d $branchesFile
-    fi
-    n=$((n + 1))
-  done <$branchesFile
-
-  echo "Branches available for $1:"
-  n=1
-  while read line; do
-    if [[ $line != *"*"* ]]; then
-      echo "$n. ${RED}$line${NOCOLOR}"
-      n=$((n + 1))
-    fi
-  done <$branchesFile
-
-  printf 'Branch number: '
-  read tmp
-  branch=$(head -$tmp $branchesFile | tail -1 | awk '{$1=$1};1')
-
-  case $1 in
-  "remote checkout")
-    git checkout -t $branch
-    ;;
-  "checkout")
-    git checkout $branch
-    ;;
-  "deletion")
-    printf "${RED}Are you sure you want to delete branch ${NOCOLOR}$branch${RED}? [y/n]${NOCOLOR} "
-
-    read confirm
-    case "$confirm" in
-    [yY]) git branch -D $branch ;;
-    *) echo "¯\_(ツ)_/¯" ;;
-    esac
-    ;;
-  esac
-}
-
-howToCommit() {
-  echo "
-  ##### Make proper commit messages #####
-
-  A properly formed git commit subject line should always be able to complete the following sentence:
-  'If applied, this commit will <your subject line here>'
-
-  ### Rules for a great git commit message style ###
-
-  - Separate subject from body with a blank line
-  - Do not end the subject line with a period
-  - Capitalize the subject line and each paragraph
-  - Use the imperative mood in the subject line
-  - Wrap lines at 72 characters
-  - Use the body to explain what and why you have done something. In most cases, you can leave out details about how a change has been made.
-
-  Refererence for more details:
-  https://gist.github.com/robertpainsi/b632364184e70900af4ab688decf6f53#a-properly-formed-git-commit-subject-line-should-always-be-able-to-complete-the-following-sentence
-  "
 }
